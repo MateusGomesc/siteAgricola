a1d967de8e48128289a8e8eeb60178d0c5aa4611
MateusGomesc <129867943+MateusGomesc@users.noreply.github.com>
2023-09-25 16:24:48 -0300

    Clima e mensagem interativa

--- API.js
+++ API.js
@@ -24,9 +24,30 @@ function getPositionSucess(position){
         var dados = await getWeatherData(latitude, longitude, apiKey)
         
         //Recebe tags
+        let icon = document.getElementById('iconTempo')
+        let temperatura = document.getElementById('temp')
+        let cidade = document.getElementById('cidade')
+        let sensacaoTermica = document.getElementById('sensacaoTermica')
+        let maxMin = document.getElementById('maxMin')
+        let msg = document.getElementById('msg')
 
         //muda conteúdo
-        
+        icon.setAttribute('src', `http://openweathermap.org/img/wn/${dados.weather[0].icon}.png`)
+        temperatura.innerHTML = `${parseInt(dados.main.temp)} °C`
+        cidade.innerHTML = dados.name
+        sensacaoTermica.innerHTML = `Sensação Térmica: ${parseInt(dados.main.feels_like)} °C`
+        maxMin.innerHTML = `${parseInt(dados.main.temp_max)}° / ${parseInt(dados.main.temp_min)}°`
+
+        tempAtual = parseInt(dados.main.temp)
+        if(tempAtual <= 20){
+            msg.innerHTML = 'Que frio produtor rural!🥶 Projeta sua lavoura com os novos produtos da SjNatur!'
+        }
+        else if(tempAtual >=21 && tempAtual <=30){
+            msg.innerHTML = 'Tá tranquilo, tá favorável!😎 O Clima está ótimo pra comprar os novos produtos SjNatur!'
+        }
+        else if(tempAtual >=31){
+            msg.innerHTML = 'O calor não está de brincadeira!🥵 Proteja sua lavoura com a nova linha SjNatur!'
+        }
     }
 
     showWeatherData()
@@ -34,7 +55,13 @@ function getPositionSucess(position){
 
 //função de erro da geolocalização
 function getPositionError(error){
-    
+    //recebe tags
+    let cidade = document.getElementById('cidade')
+    let sensacaoTermica = document.getElementById('sensacaoTermica')
+
+    //muda conteudo
+    cidade.innerHTML = 'Não encontrado...'
+    sensacaoTermica = error
 }
 
 //função da geolocalização
